46cb6924a26347e7f67eec74a1b5f0cc9a233d7a
MaurizioLeombruni <maurizio.leombruni@gmail.com>
2023-01-24 17:12:05 +0100

    Aggiunta funzione di search tramite API.

--- WebApplicationTest/Controllers/PizzeriaController.cs
+++ WebApplicationTest/Controllers/PizzeriaController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using WebApplicationTest.Areas.Identity.Data;
 using WebApplicationTest.Database;
 using WebApplicationTest.Models;
 
@@ -11,13 +13,42 @@ namespace WebApplicationTest.Controllers
     public class PizzeriaController : ControllerBase
     {
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(string? search)
         {
             using (PizzeriaContext db = new PizzeriaContext())
             {
-                List<Pizza> pizzas = db.Pizzas.ToList<Pizza>();
+                List<Pizza> pizzas = new List<Pizza>();
+
+
+                if (search is null || search == "")
+                {
+                    pizzas = db.Pizzas.ToList<Pizza>();
+                }
+                else
+                {
+                    search = search.ToLower();
+
+                    pizzas = db.Pizzas.Where(pizza => pizza.Name.ToLower().Contains(search)).ToList<Pizza>();
+                }
 
                 return Ok(pizzas);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+
+            using (PizzeriaContext db = new PizzeriaContext())
+            {
+                Pizza pizza = db.Pizzas.Where(articolo => articolo.Id == id).FirstOrDefault();
+
+                if (pizza is null)
+                {
+                    return NotFound("L'articolo con questo id non è stato trovato!");
+                }
+
+                return Ok(pizza);
             }
         }
     }
